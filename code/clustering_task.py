98e2e7154919b7f2a6b2ab508e5d2dfa83d03dee
sergej <serijnh@gmail.com>
2023-10-18 16:21:16 +0200

    init

--- code/clustering_task.py
+++ code/clustering_task.py
@@ -6,10 +6,13 @@ and dimensional reduction (PCA)
 # Import modules
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 titanic_train = pd.read_csv("../data/train.csv")
 titanic_test = pd.read_csv("../data/test.csv")
-
+y_test = pd.read_csv("../data/gender_submission.csv")
+y_test = y_test.drop("PassengerId", axis=1)
 # filtering train
 
 # replace male and female with integer values
@@ -38,6 +41,33 @@ titanic_test["Age"] = titanic_test["Age"].fillna(titanic_test["Age"].mean())
 
 titanic_test["Fare"] = titanic_test["Fare"].fillna(titanic_test["Fare"].mean())  # has one NA that needs to be filled.
 
-# knn_clf=KNeighborsClassifier()
-# knn_clf.fit(churnx_train, churny_train)
-# ypred = knn_clf.predict(churnx_test) # These are the predicted output values
+scores = {}  # save scores in empty dictionary
+ypred = {}  # save predictions in emty dictionary
+
+for i in range(1, 51):
+    knn_clf = KNeighborsClassifier(n_neighbors=i)
+    knn_clf.fit(train_x, train_y)
+    ypred[i] = knn_clf.predict(titanic_test)  # These are the predicted output values
+    scores[i] = knn_clf.score(titanic_test, y_test)
+    print("Genauigkeit auf dem Testdatensatz:{:.2f}".format(knn_clf.score(titanic_test, y_test)))
+
+# plot the scores in a lineplot
+
+# Create a new figure
+fig, ax = plt.subplots()
+
+# Plot the data
+ax.plot(range(1, 51), scores.values(), marker='o')
+
+# Set title and labels
+ax.set_title('Genauigkeit für verschiedene Werte von k')
+ax.set_xlabel('Anzahl der Nachbarn (k)')
+ax.set_ylabel('Genauigkeit')
+
+# Enable the grid
+ax.grid(True)
+
+# save plot-output
+plt.savefig("../output/kmeansscoreplot.png")
+# Show the plot
+plt.show()
