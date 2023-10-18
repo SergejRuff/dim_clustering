ffe07d0056fb120cc9afd7d8c7e542ee5b198086
sergej <serijnh@gmail.com>
2023-10-18 15:02:47 +0200

    init

--- code/clustering_task.py
+++ code/clustering_task.py
@@ -7,8 +7,6 @@ and dimensional reduction (PCA)
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 
-
-
 titanic_train = pd.read_csv("../data/train.csv")
 titanic_test = pd.read_csv("../data/test.csv")
 
@@ -19,13 +17,12 @@ titanic_train = titanic_train.replace(["male", "female"], [0, 1])
 
 # drop unwanted coloumns
 
-titanic_train = titanic_train.drop(["Cabin", "Ticket", "Name", "Embarked", "PassengerID"], axis=1)
+titanic_train = titanic_train.drop(["Cabin", "Ticket", "Name", "Embarked", "PassengerId"], axis=1)
 
-titanic_train["Age"] = titanic_train["Age"].fillna().mean()
+titanic_train["Age"] = titanic_train["Age"].fillna(titanic_train["Age"].mean())
 
 train_x = titanic_train.drop("Survived", axis=1)
 train_y = titanic_train["Survived"]
-
 
 # filtering test
 
@@ -35,10 +32,11 @@ titanic_test = titanic_test.replace(["male", "female"], [0, 1])
 
 # drop unwanted coloumns
 
-titanic_test = titanic_test.drop(["Cabin", "Ticket", "Name", "Embarked", "PassengerID"], axis=1)
+titanic_test = titanic_test.drop(["Cabin", "Ticket", "Name", "Embarked", "PassengerId"], axis=1)
 
-titanic_test["Age"] = titanic_test["Age"].fillna().mean()
-titanic_test["Fare"] = titanic_test["Fare"].fillna().mean()
+titanic_test["Age"] = titanic_test["Age"].fillna(titanic_test["Age"].mean())
+
+titanic_test["Fare"] = titanic_test["Fare"].fillna(titanic_test["Fare"].mean())  # has one NA that needs to be filled.
 
 # knn_clf=KNeighborsClassifier()
 # knn_clf.fit(churnx_train, churny_train)
